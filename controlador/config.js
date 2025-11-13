7f487ffb23989fb3e329dfdcc41659de08d8f82a
fatikitty20 <baa717871@gmail.com>
2025-11-13 00:04:31 -0600

    Update config.js
    <script type="module" src="controlador/cuentas.js"></script>

--- controlador/config.js
+++ controlador/config.js
@@ -4,3 +4,4 @@ const SUPABASE_URL = "https://qsaowtibdsouszwyupvl.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFzYW93dGliZHNvdXN6d3l1cHZsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIyMDE1NDYsImV4cCI6MjA3Nzc3NzU0Nn0.xtW53Uh4qq6XeEUoz6_eiAJN-NbRAL82rJA15FRydk8"; // tu clave real
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+<script type="module" src="controlador/cuentas.js"></script>
